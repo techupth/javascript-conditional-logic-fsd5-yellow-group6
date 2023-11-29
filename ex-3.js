d4d8d6aad0d7c1b2ff19a098529e8ea9f02bf3c1
acorriaz <mikey.swx@gmail.com>
2023-11-29 11:26:12 +0700

    Add all ex

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,10 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+if (lightBulbStatus === "On" || lightBulbStatus === "Off") {
+  lightBulbStatus === "On"
+    ? console.log("Light bulb is On.")
+    : console.log("Light bulb is Off.");
+} else {
+  console.log("Please choose the correct input (On/Off)");
+}
--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,27 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+// if (lightBulbStatus === "On") {
+//     console.log("Light bulb is On.");
+//   } else if (lightBulbStatus === "Off") {
+//     console.log("Light bulb is Off.");
+//   } else if (lightBulbStatus === "Broken") {
+//     console.log("Light bulb is Broken");
+//   } else {
+//     console.log("Please choose the correct input (On/Off/Broken)");
+//   }
+
+switch (lightBulbStatus) {
+  case "On":
+    console.log("Light bulb is On.");
+    break;
+  case "Off":
+    console.log("Light bulb is Off.");
+    break;
+  case "Broken":
+    console.log("Light bulb is Broken.");
+    break;
+  default:
+    console.log("Please choose the correct input (On/Off/Broken)");
+    break;
+}
